6c0f1b081a4005e8fde392829b638f583cc0e14e
abdulazizibm <abdulazizibm68@gmail.com>
2025-10-29 18:31:02 +0100

    feat: implement entity Product for storing products in db

---
+++ product-service/src/main/java/com/abdulazizibm/product/service/data/Product.java
@@ -0,0 +1,29 @@
+package com.abdulazizibm.product.service.data;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String name;
+
+  @Column
+  private String category;
+
+  @Column
+  private Double price;
+
+}
---
+++ product-service/src/main/java/com/abdulazizibm/product/service/data/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.abdulazizibm.product.service.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  Optional<Product> findByName(String name);
+}
