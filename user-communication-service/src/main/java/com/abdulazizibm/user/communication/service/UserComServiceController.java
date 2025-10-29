d5ff378eb35292fb0aa6f42b9a73b237f69d5eb5
abdulazizibm <abdulazizibm68@gmail.com>
2025-10-29 11:46:20 +0100

    feat: add a profile to build a fat JAR in user-communication-service

--- user-communication-service/src/main/java/com/abdulazizibm/user/communication/service/UserComServiceController.java
+++ user-communication-service/src/main/java/com/abdulazizibm/user/communication/service/UserComServiceController.java
@@ -2,21 +2,24 @@ package com.abdulazizibm.user.communication.service;
 
 import static java.text.MessageFormat.format;
 
+import com.abdulazizibm.security.core.JwtService;
 import com.abdulazizibm.user.communication.service.data.User;
 import com.abdulazizibm.user.communication.service.data.UserRepository;
 import com.abdulazizibm.user.communication.service.dto.LoginRequest;
 import com.abdulazizibm.user.communication.service.dto.RegisterRequest;
-import com.abdulazizibm.user.communication.service.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller("/auth")
+@RestController
+@RequestMapping("/auth")
 public class UserComServiceController {
 
   private final UserRepository userRepository;
@@ -24,7 +27,7 @@ public class UserComServiceController {
   private final JwtService jwtService;
 
   @PostMapping("/register")
-  public ResponseEntity<String> register(RegisterRequest request) {
+  public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
     val email = request.email();
     val password = request.password();
 
@@ -62,7 +65,7 @@ public class UserComServiceController {
           .body(format("Incorrect password for user {0}", email));
 
     }
-    val userJwt = jwtService.generateToken(user);
+    val userJwt = jwtService.generateToken(user.getEmail());
     return ResponseEntity.ok(userJwt);
 
 
