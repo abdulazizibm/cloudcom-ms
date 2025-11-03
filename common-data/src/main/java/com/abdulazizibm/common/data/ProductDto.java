797ad972ae57522ca16ae089158a28375f514d65
abdulazizibm <abdulazizibm68@gmail.com>
2025-11-03 17:25:34 +0100

    feat: create module common-data for common DTOs

---
+++ common-data/src/main/java/com/abdulazizibm/common/data/ProductDto.java
@@ -0,0 +1,5 @@
+package com.abdulazizibm.common.data;
+
+public record ProductDto(String name, double price) {
+
+}
---
+++ common-data/src/main/java/com/abdulazizibm/common/data/ProductDtoIn.java
@@ -0,0 +1,5 @@
+package com.abdulazizibm.common.data;
+
+public record ProductDtoIn(String name, int quantity) {
+
+}
