89742251eb503e2d5557f0d149ca2b9544aca1c6
abdulazizibm <abdulazizibm68@gmail.com>
2025-11-05 21:25:41 +0100

    feat: create AWS SQS Client

---
+++ cart-service/src/main/java/com/abdulazizibm/cart/service/client/AwsSqsClient.java
@@ -0,0 +1,34 @@
+package com.abdulazizibm.cart.service.client;
+
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+@Configuration
+public class AwsSqsClient {
+  @Bean
+  public SqsClient sqsClient(@Value("${aws.region}") String region, @Value("${aws.endpoint}") String endpoint){
+    var builder = SqsClient.builder().region(Region.of(region));
+
+    if (!endpoint.isBlank()) {
+      // LocalStack
+      builder =
+          builder
+              .endpointOverride(URI.create(endpoint))
+              .credentialsProvider(
+                  StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")));
+    } else {
+      // real AWS Cloud
+      builder = builder.credentialsProvider(DefaultCredentialsProvider.builder().build());
+    }
+    return builder.build();
+
+  }
+
+}
