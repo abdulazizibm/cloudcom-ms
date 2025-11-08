48d79e91572fb3c456ea20d3c097ee865e4d20e6
abdulazizibm <abdulazizibm68@gmail.com>
2025-11-08 13:32:13 +0100

    feat: add findByUserEmail() in OrderRepository

--- order-service/src/main/java/com/abdulazizibm/order/service/data/OrderRepository.java
+++ order-service/src/main/java/com/abdulazizibm/order/service/data/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.abdulazizibm.order.service.data;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+  Optional<Order> findByUserEmail(@RequestParam("userEmail") String userEmail);
 
 }
