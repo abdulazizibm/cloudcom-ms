541c59e3fc6df83d1215316978c622dbf36f9e86
abdulazizibm <abdulazizibm68@gmail.com>
2025-11-08 13:36:33 +0100

    refactor: remove unnecessary methods getProducts() and getAllProducts() from ProductServiceClient

--- cart-service/src/main/java/com/abdulazizibm/cart/service/client/ProductServiceClient.java
+++ cart-service/src/main/java/com/abdulazizibm/cart/service/client/ProductServiceClient.java
@@ -4,24 +4,13 @@ import com.abdulazizibm.common.data.ProductDtoOut;
 import com.abdulazizibm.common.data.ProductDtoIn;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "product-service", url = "${PRODUCT_SERVICE_URL}")
 public interface ProductServiceClient {
 
-  @GetMapping("/product/get")
-  ProductDtoOut getProduct(@RequestParam("name") String name);
-
-  @GetMapping("/product/getAll")
-  List<ProductDtoOut> getAllProducts();
-
   @PutMapping("/product/s2s/get")
   List<ProductDtoOut> getProducts(@RequestBody List<ProductDtoIn> productDtoIns);
-
-
-
 
 }
